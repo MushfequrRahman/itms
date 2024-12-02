3d5375a17d2e1220e8cfe46202db2e461e7b4e7c
Mushfequr Rahman <me.vgithub@gmail.com>
2024-12-02 11:51:06 +0600

    leftmenu

--- application/views/admin/leftmenu.php
+++ application/views/admin/leftmenu.php
@@ -223,8 +223,244 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 			<?php if ($this->session->userdata('userid') && $this->session->userdata('user_type') == '2') { ?>
 
+				<li class="treeview">
+					<a href="#">
+						<i class="fa fa-info" aria-hidden="true"></i><span>Product</span>
+						<span class="pull-right-container">
+							<i class="fa fa-angle-left pull-right"></i>
+						</span>
+					</a>
+					<ul class="treeview-menu">
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_list"><i class="fa fa-circle-o"></i>Product List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>MPR Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_create_form"><i class="fa fa-circle-o"></i>Add MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_form"><i class="fa fa-circle-o"></i>Date Wise MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_list_form"><i class="fa fa-circle-o"></i>Date Wise MPR List</a></li>
 
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>PO Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/po_from_mpr_form"><i class="fa fa-circle-o"></i>Add PO</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_po_list_form"><i class="fa fa-circle-o"></i>Date Wise PO List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Receive Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/receive_from_mpr_form"><i class="fa fa-circle-o"></i>Add Receive</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_receive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Receive List</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_receive_form"><i class="fa fa-circle-o"></i>Date Wise Receive List</a></li>
+								<!-- <li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_sreceive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Summary List</a></li> -->
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Inventory</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_inventory_list"><i class="fa fa-circle-o"></i>Product Inventory List</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			<?php } ?>
+			<?php //endif;
+			?>
 
+			<?php if ($this->session->userdata('userid') && $this->session->userdata('user_type') == '3') { ?>
+
+				<li class="treeview">
+					<a href="#">
+						<i class="fa fa-info" aria-hidden="true"></i><span>Product</span>
+						<span class="pull-right-container">
+							<i class="fa fa-angle-left pull-right"></i>
+						</span>
+					</a>
+					<ul class="treeview-menu">
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_list"><i class="fa fa-circle-o"></i>Product List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>MPR Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_create_form"><i class="fa fa-circle-o"></i>Add MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_form"><i class="fa fa-circle-o"></i>Date Wise MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_list_form"><i class="fa fa-circle-o"></i>Date Wise MPR List</a></li>
+
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>PO Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/po_from_mpr_form"><i class="fa fa-circle-o"></i>Add PO</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_po_list_form"><i class="fa fa-circle-o"></i>Date Wise PO List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Receive Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/receive_from_mpr_form"><i class="fa fa-circle-o"></i>Add Receive</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_receive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Receive List</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_receive_form"><i class="fa fa-circle-o"></i>Date Wise Receive List</a></li>
+								<!-- <li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_sreceive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Summary List</a></li> -->
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Inventory</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_inventory_list"><i class="fa fa-circle-o"></i>Product Inventory List</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			<?php } ?>
+			<?php //endif;
+			?>
+			<?php if ($this->session->userdata('userid') && $this->session->userdata('user_type') == '4') { ?>
+
+				<li class="treeview">
+					<a href="#">
+						<i class="fa fa-info" aria-hidden="true"></i><span>Product</span>
+						<span class="pull-right-container">
+							<i class="fa fa-angle-left pull-right"></i>
+						</span>
+					</a>
+					<ul class="treeview-menu">
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_list"><i class="fa fa-circle-o"></i>Product List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>MPR Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_create_form"><i class="fa fa-circle-o"></i>Add MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_form"><i class="fa fa-circle-o"></i>Date Wise MPR</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_mpr_list_form"><i class="fa fa-circle-o"></i>Date Wise MPR List</a></li>
+
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>PO Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/po_from_mpr_form"><i class="fa fa-circle-o"></i>Add PO</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_po_list_form"><i class="fa fa-circle-o"></i>Date Wise PO List</a></li>
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Receive Info</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<?php /*?><li><a href="<?php echo base_url();?>Dashboard/po_create_form"><i class="fa fa-circle-o"></i>Add PO</a></li><?php */ ?>
+								<li><a href="<?php echo base_url(); ?>Dashboard/receive_from_mpr_form"><i class="fa fa-circle-o"></i>Add Receive</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_receive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Receive List</a></li>
+								<li><a href="<?php echo base_url(); ?>Dashboard/date_wise_receive_form"><i class="fa fa-circle-o"></i>Date Wise Receive List</a></li>
+								<!-- <li><a href="<?php echo base_url(); ?>Dashboard/mpr_wise_sreceive_list_form"><i class="fa fa-circle-o"></i>MPR Wise Summary List</a></li> -->
+							</ul>
+						</li>
+						<li class="treeview">
+							<a href="#">
+								<i class="fa fa-id-card" aria-hidden="true"></i> <span>Product Inventory</span>
+								<span class="pull-right-container">
+									<i class="fa fa-angle-left pull-right"></i>
+								</span>
+							</a>
+							<ul class="treeview-menu">
+								<li><a href="<?php echo base_url(); ?>Dashboard/product_inventory_list"><i class="fa fa-circle-o"></i>Product Inventory List</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			<?php } ?>
+			<?php //endif;
+			?>
+
+			<?php if ($this->session->userdata('userid') && $this->session->userdata('user_type') == '5') { ?>
 
 				<li class="treeview">
 					<a href="#">
